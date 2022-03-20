d46c68adb58bdcf56ded27f862a0a4100ff41828
Felipe <machadofelipe2016@outlook.com>
2022-03-20 19:30:07 -0300

    c0- adding index principles

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const api = express()
+const port  = process.env.PORT || 3000
+const path = require('path')
+
+const mongodb = process.env.MONGODB || 'mongodb://localhost/noticia'
+const mongoose = require('mongoose')
+mongoose.Promise =  global.Promise
+
+api.get('/', (req, res) => res.render(''))
+api.set('views', path.join(__dirname, 'views'))
+api.set('view engine','ejs')
+api.use(express.static(path.join( __dirname ,'views')))
+
+
+mongoose.connect(mongodb, {useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+.then(() => {
+  
+    api.listen(port, () => console.log('listing of port: '+ port))
+})
+.catch(e => console.log(e))
+
