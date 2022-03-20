3e0900bdbce8bfdaeea681c2d36f4c8d359fec72
Felipe <machadofelipe2016@outlook.com>
2022-03-20 20:45:56 -0300

    c6- adding new test about hash and gash

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const api = express()
 const port  = process.env.PORT || 3000
 const path = require('path')
 
+
 const mongodb = process.env.MONGODB || 'mongodb://localhost/noticia'
 const mongoose = require('mongoose')
 mongoose.Promise =  global.Promise
@@ -11,6 +12,7 @@ api.get('/', (req, res) => res.render(''))
 api.set('views', path.join(__dirname, 'views'))
 api.set('view engine','ejs')
 api.use(express.static(path.join( __dirname ,'views')))
+
 
 
 mongoose.connect(mongodb, {useNewUrlParser:true,
@@ -22,3 +24,12 @@ mongoose.connect(mongodb, {useNewUrlParser:true,
 })
 .catch(e => console.log(e))
 
+
+//testing create new user to addin salt and hash of password
+const UserModel = require('./models/user')
+
+const user = new UserModel({
+    username:"felipemartins",
+    password:'felipe18'
+})
+user.save(()=> console.log('opa'))
